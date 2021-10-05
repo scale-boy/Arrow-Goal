ccead94192870370f8b35cc1dd1dc8cfc822f0a5
thuannv-1108 <nguyen.van.thuan-c@sun-asterisk.com>
2021-10-05 08:43:41 +0700

    Ogernized Base of Project

---
+++ ArrowGoal/ArrowGoal/App Delegate/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  ArrowGoal
+//
+//  Created by Thuan Nguyen Van on 05/10/2021.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
--- ArrowGoal/ArrowGoal/Scene/Login/LoginViewController.swift
+++ ArrowGoal/ArrowGoal/Scene/Login/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  ArrowGoal
 //
 //  Created by Thuan Nguyen Van on 05/10/2021.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
